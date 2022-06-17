b23635371805464729e0050e89d391c019f6b831
Porfirio Chávez <hola@porfi.dev>
2022-06-16 20:34:11 -0500

    feat: Add schema.org

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,27 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+const schema = {
+  '@context': 'https://schema.org/',
+  '@type': 'Person',
+  name: 'Porfirio Chávez (porfidev)',
+  url: 'https://porfi.dev',
+  image: 'https://secure.gravatar.com/avatar/05870bf67ffda554c989ba6134265262?s=400',
+  sameAs: [
+    'https://twitter.com/porfidev',
+    'https://www.instagram.com/porfidev/',
+    'https://www.youtube.com/channel/UCwwC9Sd1AWvoKmrAhXBPyLQ',
+    'https://www.linkedin.com/in/porfidev/',
+    'https://github.com/porfidev',
+    'https://porfi.dev',
+  ],
+  jobTitle: 'UX Engineer',
+  worksFor: {
+    '@type': 'Organization',
+    name: 'Octopy',
+  },
+};
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -10,6 +31,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400&display=swap"
             rel="stylesheet"
           />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+          />
         </Head>
         <body>
           <Main />
@@ -18,6 +43,7 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
+
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
